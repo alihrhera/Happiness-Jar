7b1a914a21fe275d6adc86a4abb13ed4ea820821
alihrhera <alihrhera1@gmail.com>
2020-12-25 02:47:26 +0200

    Update Project structure UI - Data - Util

---
+++ app/src/main/java/era/apps/happinessjar/data/locale/messages/MessageDao.kt
@@ -0,0 +1,38 @@
+package era.apps.happinessjar.data.locale.messages
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+import era.apps.happinessjar.data.models.AppMessage
+
+
+@Dao
+interface MessageDao {
+    @Insert   // insert message to database
+    fun insert( message:AppMessage)
+
+    @Update   // update message
+    fun update( message:AppMessage)
+
+    @Delete    // delete single message
+    fun delete( message:AppMessage)
+
+    // get all app message from database witch type = message
+    @Query("Select * from Message where type='message'")
+    fun getAllAppMessage():LiveData<List<AppMessage>>
+
+    // get all app message from database witch type = whatsApp
+    @Query("Select * from Message where type='WhatsApp'")
+    fun getAllAppWhatsApp() :LiveData<List<AppMessage>>
+
+    // get all app message from database witch is liked by user
+    @Query("Select * from Message where status=1")
+    fun getAllLikesMessage():LiveData<List<AppMessage>>
+
+
+}
---
+++ app/src/main/java/era/apps/happinessjar/data/locale/stories/StoryDao.kt
@@ -0,0 +1,22 @@
+package era.apps.happinessjar.data.locale.stories
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import era.apps.happinessjar.data.models.Story
+
+@Dao
+interface StoryDao {
+    @Insert
+    fun  // insert message to database
+            insert(message: Story)
+
+    // get all app message from database witch type = message
+    @get:Query("Select * from Story")
+    val allStory: LiveData<List<Story>>
+
+    @get:Query("SELECT COUNT('id') FROM Story")
+    val count: Int
+}
